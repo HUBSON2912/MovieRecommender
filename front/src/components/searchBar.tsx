96d93de8d4378207b401229ec808bf9f7734e571
Hubert Słowikowski <hubert.slowikowski.2912@gmail.com>
2026-09-03 12:37:42 +0200

    * searchBar in MUI

--- front/src/components/searchBar.tsx
+++ front/src/components/searchBar.tsx
@@ -3,10 +3,17 @@ import "../css/searchBar.css";
 import { TextField } from "@mui/material";
 
 export default function SearchBar({ value, onChange }:
-    { value: string, onChange: (event:React.ChangeEvent<HTMLInputElement>) => void }
+    { value: string, onChange: (event: React.ChangeEvent<HTMLInputElement>) => void }
 ) {
     return (
-        <TextField variant="filled" type="text" value={value} onChange={onChange} />
-        // <input className="bar" name="searchBar" value={value} onChange={onChange}></input>
+        <TextField
+            variant="filled"
+            type="text"
+            value={value}
+            onChange={onChange}
+            sx={{ width: 0.3 }}
+            slotProps={{ input: { sx: { fontSize: 22 } } }}
+            placeholder="Search a movie"
+        />
     );
 }
