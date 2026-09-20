e9d2197c6677ffbebfbc00abe4600ef11141d89c
Hubert Słowikowski <hubert.slowikowski.2912@gmail.com>
2026-09-20 13:49:44 +0200

    + destination dir path as const in download_data.sh

--- back/data/download_data.sh
+++ back/data/download_data.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -L -o ./the-movies-dataset.zip https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
+DATA_DIR="./back/data"
+
+curl -L -o "$DATA_DIR/the-movies-dataset.zip" https://www.kaggle.com/api/v1/datasets/download/rounakbanik/the-movies-dataset
